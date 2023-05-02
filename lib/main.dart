fc353837eb6f6dd30af8e85e8089c2359d7b1fce
MathieuCoding <polletmathieu.pro@gmail.com>
2023-05-02 14:13:10 +0200

    top section done

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 // import 'package:google_fonts/google_fonts.dart';
 
+const dGreen = Color(0xFF54D3C2);
+
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Hotels Booking',
-      home: Placeholder(),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: MyAppBar(),
+    );
+  }
+}
+
+class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const MyAppBar({super.key});
+  Size get preferredSize => new Size.fromHeight(50);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: IconButton(
+        onPressed: null,
+        icon: Icon(Icons.arrow_back, color: Colors.grey[800], size: 20),
+      ),
+      title: Text(
+        'Explore',
+        style: GoogleFonts.nunito(
+          color: Colors.black,
+          fontSize: 22,
+          fontWeight: FontWeight.w800,
+        ),
+      ),
+      actions: [
+        IconButton(
+          icon: Icon(
+            Icons.favorite_outline_rounded,
+            color: Colors.grey[800],
+            size: 20,
+          ),
+          onPressed: null,
+        ),
+        IconButton(
+          icon: Icon(
+            Icons.place,
+            color: Colors.grey[800],
+            size: 20,
+          ),
+          onPressed: null,
+        ),
+      ],
+      backgroundColor: Colors.white,
     );
   }
 }
